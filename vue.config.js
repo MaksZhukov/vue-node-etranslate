f531a14a8fc8c81648be0ea5d9b973ed989a751e
MaksZhukov <maks_zhukov_97@mail.ru>
2020-10-03 15:49:44 +0300

    added debug script for client

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
+const config = require("config");
+
+const { server } = config;
 module.exports = {
-  transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/],
-  productionSourceMap: false,
-  devServer: {
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-      },
+    transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/],
+    productionSourceMap: false,
+    devServer: {
+        port: 3001,
+        proxy: {
+            "/api": {
+                target: server.origin,
+            },
+        },
     },
-  },
 };
