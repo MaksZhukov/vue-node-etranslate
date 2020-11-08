8a6b4a5a399afe1fce007fdd5b73884e329c2f4e
MaksZhukov <maks_zhukov_97@mail.ru>
2020-11-08 22:50:04 +0300

    Removed welcome page

--- client/src/pages/Info.vue
+++ client/src/pages/Info.vue
@@ -28,6 +28,6 @@
 
 <script>
 export default {
-    name: 'welcome',
+    name: 'info',
 };
 </script>
--- client/src/router.js
+++ client/src/router.js
@@ -1,79 +1,73 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 // eslint-disable-next-line import/no-cycle
-import store from "./store";
-import { checkItemsInLocalStorage, setItemsToLocalStorage } from "./helpers";
+import store from './store';
+import { checkItemsInLocalStorage, setItemsToLocalStorage } from './helpers';
 
-const Welcome = () => import("./pages/Welcome.vue");
-const NotFound = () => import("./pages/NotFound.vue");
-const SignIn = () => import("./pages/SignIn.vue");
-const SignUp = () => import("./pages/SignUp.vue");
-const Dashboard = () => import("./pages/Dashboard.vue");
-const Dictionary = () => import("./pages/Dictionary.vue");
-const Recover = () => import("./pages/Recover.vue");
-const Info = () => import("./pages/Info.vue");
-const EmailForRecover = () => import("./pages/EmailForRecover.vue");
+const NotFound = () => import('./pages/NotFound.vue');
+const SignIn = () => import('./pages/SignIn.vue');
+const SignUp = () => import('./pages/SignUp.vue');
+const Dashboard = () => import('./pages/Dashboard.vue');
+const Dictionary = () => import('./pages/Dictionary.vue');
+const Recover = () => import('./pages/Recover.vue');
+const Info = () => import('./pages/Info.vue');
+const EmailForRecover = () => import('./pages/EmailForRecover.vue');
 
 Vue.use(Router);
 
 const router = new Router({
-    mode: "history",
+    mode: 'history',
     routes: [
         {
-            path: "/",
-            name: "welcome",
-            component: Welcome,
-        },
-        {
-            path: "/sign-in",
-            name: "signIn",
+            path: '/sign-in',
+            name: 'signIn',
             component: SignIn,
             meta: {
                 user: false,
             },
         },
         {
-            path: "/sign-up",
-            name: "signUp",
+            path: '/sign-up',
+            name: 'signUp',
             component: SignUp,
             meta: {
                 user: false,
             },
         },
         {
-            path: "/dashboard",
-            name: "dashboard",
+            path: '/dashboard',
+            name: 'dashboard',
             component: Dashboard,
             meta: {
                 user: true,
             },
         },
         {
-            path: "/dictionary",
-            name: "dictionary",
+            path: '/dictionary',
+            name: 'dictionary',
             component: Dictionary,
             meta: {
                 user: true,
             },
         },
         {
-            path: "/info",
-            name: "info",
+            path: '/info',
+            name: 'info',
             component: Info,
             meta: {
                 user: true,
             },
         },
         {
-            path: "/recover/:recoverToken",
-            name: "recover",
+            path: '/recover/:recoverToken',
+            name: 'recover',
             component: Recover,
             meta: {
                 user: false,
             },
             beforeEnter: async (to, from, next) => {
                 await store.dispatch(
-                    "userModule/checkRecoverToken",
+                    'userModule/checkRecoverToken',
                     to.params.recoverToken
                 );
                 const {
@@ -86,38 +80,38 @@ const router = new Router({
                 if (access) {
                     next();
                 } else {
-                    next("/sign-in");
+                    next('/sign-in');
                 }
             },
         },
         {
-            path: "/email-for-recover",
-            name: "emailForRecover",
+            path: '/email-for-recover',
+            name: 'emailForRecover',
             component: EmailForRecover,
             meta: {
                 user: false,
             },
         },
         {
-            path: "/404",
-            name: "404",
+            path: '/404',
+            name: '404',
             component: NotFound,
         },
         {
-            path: "*",
-            redirect: "/404",
+            path: '*',
+            redirect: '/404',
         },
     ],
 });
 
 router.beforeEach(async (to, from, next) => {
     let isTokensAndExpiresIn = checkItemsInLocalStorage([
-        "refreshToken",
-        "accessToken",
-        "expiresIn",
+        'refreshToken',
+        'accessToken',
+        'expiresIn',
     ]);
     if (
-        to.path === "/" &&
+        to.path === '/' &&
         !isTokensAndExpiresIn &&
         Object.keys(to.query).length > 0
     ) {
@@ -133,24 +127,24 @@ router.beforeEach(async (to, from, next) => {
     } = store;
     let access;
     if (!isTokensAndExpiresIn && user !== null) {
-        store.commit("userModule/logOut");
+        store.commit('userModule/logOut');
         return;
     }
-    await store.dispatch("userModule/tryUpdateTokens");
+    await store.dispatch('userModule/tryUpdateTokens');
     if (user && user.id) {
         access = true;
     } else if (
         Object.keys(checkTokenResponse).length === 0 &&
         isTokensAndExpiresIn
     ) {
-        await store.dispatch("userModule/checkToken");
+        await store.dispatch('userModule/checkToken');
         access = true;
     }
     if (!isLoad) {
-        store.commit("loaded");
+        store.commit('loaded');
     }
     if (to.meta.user && !access) {
-        next("sign-in");
+        next('sign-in');
         return;
     }
     if (to.meta.user === false && access) {
